06fdc78265f2ca64940a2cc715e7d23cb7b4fc94
Maksyymm23 <ponomarovmaksim23@gmail.com>
2025-03-06 20:13:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+/* https://greensock.com/gsap */
+TweenLite.set("#petals", { perspective:600 })
+TweenLite.set("img", { xPercent:"-50%", yPercent:"-50%" })
+
+var total = 50;
+var warp = document.getElementById("petals"),
+w = window.innerWidth,
+h = window.innerHeight;
+
+for (i = 0; i < total; i++) {
+    var Div = document.createElement('div');
+    TweenLite.set(Div, { attr: { class:'dot' }, x:R(0,w), y:R(-200,-150), z:R(-200,200)});
+    warp.appendChild(Div);
+    animm(Div);
+}
+
+function animm(elm) {
+    TweenMax.to(elm,R(6,15), { y:h+100, ease:Linear.easeNone, repeat:-1, delay:-15 });
+    TweenMax.to(elm,R(4,8), { x:'+=100', rotationZ:R(0,180), repeat:-1, yoyo:true, ease:Sine.easeInOut });
+    TweenMax.to(elm,R(2,8), { rotationX:R(0,360), rotationY:R(0,360), repeat:-1, yoyo:true, ease:Sine.easeInOut, delay:-5 });
+};
+
+function R(min,max) { return min+Math.random() * (max-min) };
+
+
+/* https://mattboldt.com/typed.js/ */
+var typed = new Typed('#text', {
+    strings: ['С 8 Марта, солнышко! Ты у меня самая красивая и невероятная, и я хочу, чтобы сегодня ты улыбалась ещё больше. Пусть весна подарит тебе кучу радости, волшебства и самых тёплых моментов. Обожаю тебя! 💐💖'],
+    startDelay: 3000,
+    typeSpeed: 50,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: false,
+    showCursor: false,
+    onComplete: function() {
+        var author = document.getElementById("author");
+        author.style.opacity = 1;
+    }
+});
